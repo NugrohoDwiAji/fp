2492b440215eac75d325a36324514ec94196aa17
NugrohoDwiAji <nugrohodwiaji@gmail.com>
2025-06-24 10:54:31 +0800

    memperbaiki fungsi menambahkan identitas

--- src/pages/api/identitas.ts
+++ src/pages/api/identitas.ts
@@ -10,67 +10,80 @@ type DataIdentitas = {
   value: string[];
 };
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-const createUploadDir = (dir: string) => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-};
+// const createUploadDir = (dir: string) => {
+//   if (!fs.existsSync(dir)) {
+//     fs.mkdirSync(dir, { recursive: true });
+//   }
+// };
 
 // POST
 const handlePostMethod = async (req: NextApiRequest, res: NextApiResponse) => {
-  createUploadDir(path.join(process.cwd(), "/public/img"));
 
-  const form = formidable({
-    uploadDir: path.join(process.cwd(), "public", "img"),
-    filename: (_, __, part, ___) => {
-      return `${part.originalFilename}`;
-    },
-  });
-
+  const {name,value} = req.body
   try {
-    const { fields, files } = await new Promise<{
-      fields: Fields;
-      files: Files;
-    }>((resolve, reject) => {
-      form.parse(req, (err, fields, files) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve({ fields, files });
-      });
-    });
-
-    const { title, value } = fields as DataIdentitas;
-
-    console.log(value);
-    console.log(title[0]);
-    const file = Array.isArray(files.value) ? files.value[0] : files.value;
-    const filePath = `/img/${file?.originalFilename}`;
-    let valued: string = "";
-
-    if (value === undefined) {
-      valued = filePath;
-    } else {
-      valued = value[0];
-    }
-
-    const saveData = await prisma.identitas.create({
+    const result = await prisma.identitas.create({
       data: {
-        name: title.toString(),
-        value: valued,
+        name: name,
+        value: value,
       },
-    });
-    res.status(202).json({ massage: "Success" });
+    })
+    res.status(202).json(result);
   } catch (error) {
-    res.status(500).json({ error: "Error saving file" });
+    res.status(500).json({ error: "Error creating content" });  
   }
+  // createUploadDir(path.join(process.cwd(), "/public/img"));
+
+  // const form = formidable({
+  //   uploadDir: path.join(process.cwd(), "public", "img"),
+  //   filename: (_, __, part, ___) => {
+  //     return `${part.originalFilename}`;
+  //   },
+  // });
+
+  // try {
+  //   const { fields, files } = await new Promise<{
+  //     fields: Fields;
+  //     files: Files;
+  //   }>((resolve, reject) => {
+  //     form.parse(req, (err, fields, files) => {
+  //       if (err) {
+  //         reject(err);
+  //         return;
+  //       }
+  //       resolve({ fields, files });
+  //     });
+  //   });
+
+  //   const { title, value } = fields as DataIdentitas;
+
+  //   console.log(value);
+  //   console.log(title[0]);
+  //   const file = Array.isArray(files.value) ? files.value[0] : files.value;
+  //   const filePath = `/img/${file?.originalFilename}`;
+  //   let valued: string = "";
+
+  //   if (value === undefined) {
+  //     valued = filePath;
+  //   } else {
+  //     valued = value[0];
+  //   }
+
+  //   const saveData = await prisma.identitas.create({
+  //     data: {
+  //       name: title.toString(),
+  //       value: valued,
+  //     },
+  //   });
+  //   res.status(202).json({ massage: "Success" });
+  // } catch (error) {
+  //   res.status(500).json({ error: "Error saving file" });
+  // }
 };
 
 // GET
